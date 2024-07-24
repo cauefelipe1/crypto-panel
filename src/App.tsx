c137458bd06a81880ddac3ebead928174aed9663
Caue Carvalho <cauefelipe1@hotmail.com>
2024-07-24 17:42:51 -0300

    Adding a few routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,23 @@ import './App.css';
 import 'primereact/resources/themes/mira/theme.css';
 
 import CryptoPanelPage from './pages/CryptoPanelPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterUserPage from './pages/RegisterUserPage';
 
 export default function App() {
 
   return (
-    <div className="App">
-      <CryptoPanelPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register-user' element={<RegisterUserPage />} />
+        <Route path='/panel' element={<CryptoPanelPage />} />
+        {/* <div className="App">
+          <CryptoPanelPage />
+        </div> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/RegisterUserPage.tsx
@@ -0,0 +1,7 @@
+export default function RegisterUserPage() {
+  return (
+    <>
+      <p>Register page in progress.</p>
+    </>
+  );
+}
