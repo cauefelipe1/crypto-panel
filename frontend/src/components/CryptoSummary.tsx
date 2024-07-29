5567ca4ce8e1e11b52c95b6bb89c1fe1b47781ae
Caue Carvalho <cauefelipe1@hotmail.com>
2024-07-29 14:08:13 -0300

    Movin the project to Vite

---
+++ frontend/src/components/CryptoSummary.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { CryptoModel } from "../models/Crypto";
+import { InputNumber } from "primereact/inputnumber"
+
+export type Props = {
+    crypto: CryptoModel;
+    updateOwner: (crypto: CryptoModel, amount: number) => void;
+}
+
+export default function CryptoSummary({crypto, updateOwner} : Props) : JSX.Element {
+    const [amount, setAmount] = useState(0);
+
+    useEffect(function initialOwnerUpdate() {
+        updateOwner(crypto, amount);
+    }, []);
+
+    return (
+        <div>
+            <span>{crypto.name + " $" + crypto.current_price}</span>
+
+            <InputNumber
+                className="crypto-summary-input"
+                value={amount}
+                mode="decimal"
+                showButtons
+                step={1}
+                onChange={(e) => {
+                    const amt = e.value ?? 0
+
+                    setAmount(amt);
+                    updateOwner(crypto, amt);
+                }}
+            />
+            <p>$ {(crypto.current_price * (amount ?? 0)).toLocaleString(undefined, {minimumFractionDigits: 2 , maximumFractionDigits: 2})}</p>
+        </div>
+    );
+}
